b6e4ce305d8bfd5a639a08823bc905116db731ff
rbazelais <bazelais.rachel@gmail.com>
2018-03-07 12:54:50 -0800

    Math Module - requires ./practice

---
+++ Node/Math_Module/app.js
@@ -0,0 +1,6 @@
+var quick_maths = require('./mathlib');
+console.log(quick_maths.add(2,2));
+console.log(quick_maths.multiply(2,10));
+console.log(quick_maths.square(12));
+console.log(quick_maths.random(1,10))
+;
