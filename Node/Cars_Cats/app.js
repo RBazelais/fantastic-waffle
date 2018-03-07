8875a8d6a98794b532408143bf219e6350c8bcb2
rbazelais <bazelais.rachel@gmail.com>
2018-03-07 12:14:09 -0800

    Cars and Cats + Image uploader form

--- Node/Cars_Cats/app.js
+++ Node/Cars_Cats/app.js
@@ -8,32 +8,81 @@ var server = http.createServer(function (request, response){
     console.log('client request URL: ', request.url);
     // this is how we do routing:
     if(request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors, contents){
+        fs.readFile('./views/index.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents); 
             response.end();
         });
     }
     else if (request.url === "/cars") {
-         fs.readFile('cars.html', 'utf8', function (errors, contents){
+         fs.readFile('./views/cars.html', 'utf8', function (errors, contents){
              response.writeHead(200, {'Content-type': 'text/html'});
              response.write(contents); 
              response.end();
          });
     }
-    else if (request.url === "/cats") {
-        fs.readFile('cats.html', 'utf8', function (errors, contents){
+    else if (request.url === '/images/aston-martin.jpg') {
+        fs.readFile('./images/aston-martin.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/aston-martin.jpg') {
+        fs.readFile('./images/aston-martin.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/dodge-70-charger.jpg') {
+        fs.readFile('./images/dodge-70-charger.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/somecars.jpg') {
+        fs.readFile('./images/somecars.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/cars/new') {
+        fs.readFile('./views/new.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-type': 'text/html'});
             response.write(contents); 
             response.end();
         });
     }
-    else if (request.url === "/cars/new") {
-        fs.readFile('cats.html', 'utf8', function (errors, contents){
+    else if (request.url === '/cats') {
+        fs.readFile('./views/cats.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-type': 'text/html'});
             response.write(contents); 
             response.end();
         });
+    }
+    else if (request.url === '/images/gizmo.jpg') {
+        fs.readFile('./images/gizmo.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/IMG_20160318_211225.jpg') {
+        fs.readFile('./images/IMG_20160318_211225.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/IMG_20170628_190213.jpg') {
+        fs.readFile('./images/IMG_20170628_190213.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
     }
     else if(request.url === '/stylesheets/style.css'){
         fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
