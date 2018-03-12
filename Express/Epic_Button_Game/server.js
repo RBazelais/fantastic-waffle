625b66c25b9a2608f57f7403d23f5df44662ff4e
rbazelais <bazelais.rachel@gmail.com>
2018-03-12 09:32:42 -0700

    Epic Button Game

---
+++ Express/Epic_Button_Game/server.js
@@ -0,0 +1,48 @@
+// Import express and path modules.
+var express = require( "express");
+var path = require( "path");
+
+// Create the express app.
+var app = express();
+
+// Define the static folder.
+app.use(express.static(path.join(__dirname, "./static")));
+
+// Setup ejs templating and define the views folder.
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// Root route to render the index.ejs view.
+app.get('/', function(req, res) {
+	console.log("**************")
+	console.log("get")
+	var num = 0;
+	num++;
+ 	res.render("index", {num:num});
+})
+
+// tell the express app to listen on port 8001
+var server = app.listen(8000, function() {
+	console.log("listening on port 8000");
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+	console.log("Client/socket is connected!");
+	console.log("Client/socket id is: ", socket.id);
+	// all the server socket code goes in here
+
+	//recieve data from the form
+	socket.on("increment_num", function(data){
+		// console.log(data);
+		//create a message with that data
+		let num = data;
+		var msg = `The button has be pushed ${data} time(s)`;
+
+		//emit message to client
+		socket.emit("send_back_number", msg);
+		console.log("******* server: \n" + msg);
+		
+	})
+})
+
--- Express/Great_Number_Game/server.js
+++ Express/Great_Number_Game/server.js
@@ -1,44 +1,50 @@
-// require express
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const session = require('express-session');
+const app = express();
+const bodyParser = require('body-parser');
 
-// path module -- try to figure out where and why we use this
-var path = require("path");
-
-// create the express app
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'supersecretSupersafe' }));
+app.use(express.static(path.join(__dirname, "../")));
 
-// setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-// root route to render the index.ejs view
 app.get('/', function (req, res) {
-	res.render("index");
+    console.log("*****", req.session, "\nEnd Session *****");
+    context = req.session;
+    res.render("index", context);
+})
+app.post('/', function (req, res) {
+    if (req.body.NewGame){
+        req.session.status = 'reset';
+        req.session.result = 'reset';
+    }
+    if (req.session.status != "inGame"){
+        console.log("***********************\nNewGame\n ***********************");
+        req.session.status = "inGame"; 
+        req.session.number = Math.floor(Math.random() * (100 - 1)) + 1;
+        req.session.guess = req.body.guess;
+    }
+    var guess = req.body.guess;
+    console.log("the user guess is:", guess);
+    var number = req.session.number;
+    if(guess > number){
+        req.session.result = "Greater";
+    }
+    else if (guess < number){
+        req.session.result = "Less";
+    }
+    else if (guess == number) {
+        req.session.result = "Win";
+    };
+    req.session.guess = guess;
+    console.log("session guess is:", req.session.guess)
+    console.log("game state is:", req.session.status)
+    res.redirect("/");
 })
 
-var count = 0;
-// root route to render the result.ejs view
-app.post('/result', function (req, res) {
-	var name = req.body.name;
-	var location = req.body.location;
-	var language = req.body.language;
-	var comment = req.body.comment;
-	count++;
-	console.log("POST DATA", req.body);
-	res.render('result', {
-		name: name,
-		location: location,
-		language: language,
-		comment: comment,
-		count: count
-	})
-})
-
- 
-// tell the express app to listen on port 8000
 app.listen(8000, function () {
-	console.log("listening on port 8000");
+    console.log("listening on port 8000");
 });
